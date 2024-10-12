902a8d9f2baeeb830364f5d15f25f09fed123f5e
Jisun-K <kjisun1221@gmail.com>
2024-10-12 16:38:55 +0900

    ** 2024.10.12(2) + home style + move about btn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'anguar_no-standalone';
+  //  router 와 같은 이름의 객체를 갖지고 있는 배열 선언
+  // 전체적인 스크롤 이벤트를 받아 아래, 위에 맞는 routerLink 보내기 
 }
