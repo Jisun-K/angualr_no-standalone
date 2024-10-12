3b8b5a4df7f83671b970ebbc3bf9cf39ab932f25
Jisun-K <kjisun1221@gmail.com>
2024-10-12 13:23:32 +0900

    ** 2024.10.12  + project angular-no-standalone  + project 구조  + rest css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // redirect to `first-component`
+  { path: 'home', component: HomeComponent },
+  // { path: 'second-component', component: SecondComponent },
+  // { path: '**', component: PageNotFoundComponent },
+
+  // {path: 'hero/:id', component: HeroDetailComponent},
+  // {
+  //   path: 'first-component',
+  //   title: 'First component',
+  //   component: FirstComponent,  // this is the component with the <router-outlet> in the template
+  //   children: [
+  //     {
+  //       path: 'child-a',  // child route path
+  //       title: resolvedChildATitle,
+  //       component: ChildAComponent,  // child route component that the router renders
+  //     },
+  //   ],
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
